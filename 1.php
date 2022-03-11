4d185ee196a215a212cb514451f235ff683243d6
jacky <a0970838302@gmail.com>
2022-03-11 18:09:22 +0800

    0311

---
+++ 1.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [
+    [
+        'id' => '301',
+        'league' => 'NBA',
+        'team_a' => 'A',
+        'team_b' => 'B',
+        'ratio_win' => '1.00',
+        'ratio_lose' => '1.01',
+    ],
+    // .....
+];
+
+var_dump($arr);
